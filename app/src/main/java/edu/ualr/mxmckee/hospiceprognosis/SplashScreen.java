22fd592d887d00a3d347c23ba8fd5c688d831792
mxmckee <41008554+mxmckee@users.noreply.github.com>
2019-11-27 21:31:31 -0600

    Added support for splash screen.

---
+++ app/src/main/java/edu/ualr/mxmckee/hospiceprognosis/SplashScreen.java
@@ -0,0 +1,25 @@
+package edu.ualr.mxmckee.hospiceprognosis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EasySplashScreen config = new EasySplashScreen(SplashScreen.this)
+                .withTargetActivity(MainActivity.class)
+                .withSplashTimeOut(3000)
+                .withLogo(R.drawable.splash_screen_logo);
+
+        View view = config.create();
+
+        setContentView(view);
+    }
+}
